fc3bc2502fc5310bbcf1bd27d9a7fab40d4c78bf
Alfariz 11 <ralfariz780@gmail.com>
2025-10-05 16:33:15 +0700

    feat: Enhance user management and authentication flow with role-based routes and improved error handling

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,24 @@ require __DIR__ . '/kasir.php';
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/karyawan/scanner', function () {
+    return Inertia::render('Karyawan/Scanner');
+})->name('scanner');
+
+// Halaman login (GET)
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+
+// Proses login (POST)
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Logout (POST)
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// // Profil pengguna yang sedang login
+// Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth')->name('profile');
+
+
+require __DIR__ . '/admin.php';
+require __DIR__ . '/karyawan.php';
+require __DIR__ . '/kasir.php';
