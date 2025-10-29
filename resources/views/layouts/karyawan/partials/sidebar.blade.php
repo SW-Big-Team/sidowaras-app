19760670567ccb637a3d077bc83ae165805d3022
Rizki Alfariz Ramadhan <rizki.122140061@student.itera.ac.id>
2025-10-29 23:10:57 +0700

    feat: update sidebar navigation to include 'Manajemen Obat' section and restore 'Pembelian' link across admin, karyawan, and kasir layouts

--- resources/views/layouts/karyawan/partials/sidebar.blade.php
+++ resources/views/layouts/karyawan/partials/sidebar.blade.php
@@ -17,12 +17,9 @@
         </a>
       </li>
 
-      {{-- Pembelian --}}
-      <li class="nav-item">
-          <a class="nav-link {{ request()->routeIs('pembelian.*') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('pembelian.index') }}">
-              <i class="material-symbols-rounded opacity-5">dashboard</i>
-              <span class="nav-link-text">Pembelian</span>
-          </a>
+      {{-- Manajemen Obat --}}
+      <li class="nav-item mt-3">
+        <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Manajemen Obat</h6>
       </li>
 
       {{-- Obat --}}
@@ -73,6 +70,14 @@
       {{-- Stock Barang --}}
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Manajemen Stok</h6>
+      </li>
+
+      {{-- Pembelian --}}
+      <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('pembelian.*') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('pembelian.index') }}">
+              <i class="material-symbols-rounded opacity-5">dashboard</i>
+              <span class="nav-link-text">Pembelian</span>
+          </a>
       </li>
 
       <li class="nav-item">
--- resources/views/layouts/kasir/partials/sidebar.blade.php
+++ resources/views/layouts/kasir/partials/sidebar.blade.php
@@ -17,12 +17,9 @@
         </a>
       </li>
 
-      {{-- Pembelian --}}
-      <li class="nav-item">
-          <a class="nav-link {{ request()->routeIs('pembelian.*') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('pembelian.index') }}">
-              <i class="material-symbols-rounded opacity-5">dashboard</i>
-              <span class="nav-link-text">Pembelian</span>
-          </a>
+      {{-- Manajemen Obat --}}
+      <li class="nav-item mt-3">
+        <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Manajemen Obat</h6>
       </li>
 
       {{-- Obat --}}
@@ -60,6 +57,14 @@
       {{-- Transaksi --}}
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Transaksi</h6>
+      </li>
+
+      {{-- Pembelian --}}
+      <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('pembelian.*') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('pembelian.index') }}">
+              <i class="material-symbols-rounded opacity-5">dashboard</i>
+              <span class="nav-link-text">Pembelian</span>
+          </a>
       </li>
 
       {{-- POS / Transaksi --}}
